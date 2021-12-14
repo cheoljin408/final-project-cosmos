1df7ae95839f54109e0b060ed33bc86873bc1ce7
cheoljin408 <kcj409@naver.com>
2021-12-14 15:48:58 +0900

    first commit

---
+++ src/main/java/org/kosta/finalproject/CosmosApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kosta/finalproject/CosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosta.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
