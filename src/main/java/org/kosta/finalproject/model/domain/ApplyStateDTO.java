e4dccb82e2a538e5e5f95aebdbd5b9a40579459c
Joonkb <byeongjunkim@naver.com>
2021-12-16 10:44:50 +0900

    DTO 변수명 수정 및 Lombok 수정

--- src/main/java/org/kosta/finalproject/model/domain/ApplyStateDTO.java
+++ src/main/java/org/kosta/finalproject/model/domain/ApplyStateDTO.java
@@ -1,8 +1,12 @@
 package org.kosta.finalproject.model.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApplyStateDTO {
     private String applyStateCode;
     private String applyState;
--- src/main/java/org/kosta/finalproject/model/domain/StudyStateDTO.java
+++ src/main/java/org/kosta/finalproject/model/domain/StudyStateDTO.java
@@ -1,8 +1,12 @@
 package org.kosta.finalproject.model.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudyStateDTO {
     private String studyStateCode;
     private String studyState;
