1341f3161ea9e3dae3fa524a34e9ce18e09e58f8
Joonkb <byeongjunkim@naver.com>
2021-12-18 19:36:29 +0900

    COSMOS41 UPDATE
    Ajax를 이용한 모달창 띄우기

--- src/main/java/org/kosta/finalproject/controller/ApplyController.java
+++ src/main/java/org/kosta/finalproject/controller/ApplyController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,9 +22,20 @@ public class ApplyController {
     }
 
     @GetMapping("/alarm")
-    public String alarm(Model model) {
+    public String getAlarmList(Model model) {
         System.out.println(applyService.alarm());
         return "apply-alarm";
     }
 
+    /**
+     *
+     *  스터디 신청서에서 팀장이 수락 버튼을 누른경우 해당 로직을 실행하는 함수 ( Ajax 통신 )
+     *
+     * @return
+     */
+    @GetMapping("/apply")
+    @ResponseBody
+    public String applyRefuse(@RequestParam int applyNo){
+        return null;
+    }
 }
