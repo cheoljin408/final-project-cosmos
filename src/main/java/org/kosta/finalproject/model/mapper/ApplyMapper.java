ad5d3334845250c54cb1600fe50235b418d6beb8
KimJoongHyeok <moon960427@gmail.com>
2021-12-19 13:11:58 +0900

    COSMOS-43
    스터디 참가 신청 기능 구현

--- src/main/java/org/kosta/finalproject/model/mapper/ApplyMapper.java
+++ src/main/java/org/kosta/finalproject/model/mapper/ApplyMapper.java
@@ -1,6 +1,7 @@
 package org.kosta.finalproject.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
@@ -10,4 +11,8 @@ import java.util.Map;
 public interface ApplyMapper {
     //ApplyMapper.xml로 alarm
     List<Map<String, Object>> alarm();
+
+    void registerApplyStudy(@Param("studyNo") String studyNo, @Param("applyContent")String applyContent, @Param("email")String email);
+
+    int checkApplyByStudyNoAndEmail(@Param("studyNo")String studyNo, @Param("email")String email);
 }
--- src/main/java/org/kosta/finalproject/service/ApplyService.java
+++ src/main/java/org/kosta/finalproject/service/ApplyService.java
@@ -1,9 +1,12 @@
 package org.kosta.finalproject.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public interface ApplyService {
     //선언부
     List<Map<String, Object>> alarm();
+
+    int registerApplyStudy(HashMap<String, String> jsonData);
 }
