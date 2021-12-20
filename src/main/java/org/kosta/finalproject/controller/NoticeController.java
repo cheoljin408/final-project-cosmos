de0eaa815f934827491f10c23041362ba5bb2201
Yong Lee <nogy21@gmail.com>
2021-12-21 00:39:37 +0900

    COSMOS-56 공지사항 리스트 기능 구현
    리스트 조회 및 뷰에 출력 완료

--- src/main/java/org/kosta/finalproject/controller/NoticeController.java
+++ src/main/java/org/kosta/finalproject/controller/NoticeController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.kosta.finalproject.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Controller
@@ -17,8 +19,10 @@ public class NoticeController {
         this.noticeService = noticeService;
     }
 
-    @GetMapping("/notice/list")
-    public String comment() {
+    @GetMapping("/notice/list/{studyNo}")
+    public String comment(@PathVariable int studyNo, Model model) {
+        model.addAttribute("noticeList", noticeService.getAllNoticeList(studyNo));
         return "notice-list";
     }
+
 }
