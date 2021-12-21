c8a8224805b4d589453af1cab0c4a43d09955799
Joonkb <byeongjunkim@naver.com>
2021-12-21 14:11:10 +0900

    COSMOS-55 공지사항 상세보기 페이지 구현
    공지사항 상세보기 페이지 View 작업

--- src/main/java/org/kosta/finalproject/controller/NoticeController.java
+++ src/main/java/org/kosta/finalproject/controller/NoticeController.java
@@ -1,2 +1,24 @@
-package org.kosta.finalproject.controller;public class NoticeController {
+package org.kosta.finalproject.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequestMapping("/notice")
+public class NoticeController {
+
+    @RequestMapping("/detail")
+    public String noticeDetail(Model model) {
+         NoticeDTO
+         return "/notice/notice-detail";
+    }
+    @PostMapping("/post")
+    public String postTest(String text, Model model){
+        System.out.println("text = " + text );
+        model.addAttribute("data", text);
+        return "/notice/notice-result";
+    }
 }
