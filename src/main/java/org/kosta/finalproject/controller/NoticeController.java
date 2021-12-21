c2675d5467d26f67aae78fb7d6abf0aa0510fa11
westkitekim <westkite418@gmail.com>
2021-12-21 16:09:18 +0900

    COSMOS-57 푸터, 사이드바 테스트

--- src/main/java/org/kosta/finalproject/controller/NoticeController.java
+++ src/main/java/org/kosta/finalproject/controller/NoticeController.java
@@ -14,18 +14,30 @@ public class NoticeController {
 
     @GetMapping("/sidebar")
     public String sidebar() {
-        return "/fragments/sidebar";
+        return "fragments/sidebar";
     }
 
     @GetMapping("/sidebar2")
     public String sidebar2() {
-        return "/notice/sidebar-test2";
+        return "notice/sidebar-test2";
     }
 
-    //공지사항 등록
-    @PostMapping("/registerNotice")
+    //공지사항 등록페이지(Test)
+    @GetMapping("/registerNotice")
     public String registerNotice() {
-
-        return "/notice/register-notice";
+        return "notice/register-notice";
     }
+
+    //lms 사이드바 적용 공지사항 등록페이지
+    @GetMapping("/lmsRegisterNotice")
+    public String lmsRegisterNotice() {
+        return "lms/lms-register-notice";
+    }
+
+    @GetMapping("/noticelist")
+    public String noticelist() {
+        return "noticelist";
+    }
+
+
 }
