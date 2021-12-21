1343767824b462e93cb9e7cdc0cf1ae407a4f565
cheoljin408 <kcj409@naver.com>
2021-12-21 22:07:53 +0900

    COSMOS-58 LMS 메인페이지 관련 메소드 추가

--- src/main/java/org/kosta/finalproject/service/StudyMemberService.java
+++ src/main/java/org/kosta/finalproject/service/StudyMemberService.java
@@ -1,2 +1,12 @@
-package org.kosta.finalproject.service;public interface StudyMemberService {
+package org.kosta.finalproject.service;
+
+import org.kosta.finalproject.model.domain.StudyMemberDTO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StudyMemberService {
+    List<Map<String, Object>> getStudyNameList(Map<String, Object> emailAndStudyNo);
+
+    StudyMemberDTO getAllStudyInfo(int studyNo);
 }
