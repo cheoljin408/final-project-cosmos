75048ee18ea5f3b10fff82ee8424bee0a707651e
westkitekim <westkite418@gmail.com>
2021-12-21 17:28:45 +0900

    COSMOS-57 공지사항 등록 뷰페이지 생성
    NoticeController
    fragments/lms-header.html
    lms/lms-register-notice.html - cdn 추가

--- src/main/java/org/kosta/finalproject/controller/NoticeController.java
+++ src/main/java/org/kosta/finalproject/controller/NoticeController.java
@@ -7,37 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class NoticeController {
 
-    @GetMapping("/register-notice")
-    public String testRegisterNotice() {
-        return "/notice/sidebar-test";
-    }
-
-    @GetMapping("/sidebar")
-    public String sidebar() {
-        return "fragments/sidebar";
-    }
-
-    @GetMapping("/sidebar2")
-    public String sidebar2() {
-        return "notice/sidebar-test2";
-    }
-
-    //공지사항 등록페이지(Test)
-    @GetMapping("/registerNotice")
-    public String registerNotice() {
-        return "notice/register-notice";
-    }
-
-    //lms 사이드바 적용 공지사항 등록페이지
+    //LMS 사이드바 적용 공지사항 등록페이지
     @GetMapping("/lmsRegisterNotice")
     public String lmsRegisterNotice() {
         return "lms/lms-register-notice";
     }
-
-    @GetMapping("/noticelist")
-    public String noticelist() {
-        return "noticelist";
-    }
-
-
 }
