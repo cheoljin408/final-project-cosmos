c242dfd6e05c8f28877e0935ecc30b626589f449
cheoljin408 <kcj409@naver.com>
2021-12-24 00:15:50 +0900

    COSMOS-72 스터디 리스트 페이징 기능 구현

--- src/main/java/org/kosta/finalproject/service/PagingService.java
+++ src/main/java/org/kosta/finalproject/service/PagingService.java
@@ -1,2 +1,13 @@
-package org.kosta.finalproject.service;public interface PagingService {
+package org.kosta.finalproject.service;
+
+import org.kosta.finalproject.model.domain.StudyMemberDTO;
+
+import java.util.List;
+
+public interface PagingService {
+
+    int getTotalCountOfStudyList();
+
+    List<StudyMemberDTO> getStudyListByPageNo(int startRowNumber, int endRowNumber);
+
 }
--- src/main/java/org/kosta/finalproject/service/PagingServiceImpl.java
+++ src/main/java/org/kosta/finalproject/service/PagingServiceImpl.java
@@ -1,2 +1,31 @@
-package org.kosta.finalproject.service;public class PagingServiceImpl {
+package org.kosta.finalproject.service;
+
+import org.kosta.finalproject.model.domain.StudyMemberDTO;
+import org.kosta.finalproject.model.mapper.StudyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PagingServiceImpl implements PagingService{
+
+    private final StudyMapper studyMapper;
+
+    @Autowired
+    public PagingServiceImpl(StudyMapper studyMapper) {
+        this.studyMapper = studyMapper;
+    }
+
+    @Override
+    public int getTotalCountOfStudyList() {
+        int totalCount = studyMapper.getTotalCountOfStudyList();
+        return totalCount;
+    }
+
+    @Override
+    public List<StudyMemberDTO> getStudyListByPageNo(int startRowNumber, int endRowNumber) {
+        List<StudyMemberDTO> studyList = studyMapper.getStudyListByPageNo(startRowNumber, endRowNumber);
+        return studyList;
+    }
 }
