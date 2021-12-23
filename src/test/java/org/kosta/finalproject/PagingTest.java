59861ad4fdc2ab3caf6b70ddb81d8061958bad00
cheoljin408 <kcj409@naver.com>
2021-12-23 23:54:11 +0900

    COSMOS-72 페이징 테스트 코드 작성

--- src/test/java/org/kosta/finalproject/PagingTest.java
+++ src/test/java/org/kosta/finalproject/PagingTest.java
@@ -1,2 +1,31 @@
-package org.kosta.finalproject;public class PagingTest {
+package org.kosta.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.kosta.finalproject.service.PagingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PagingTest {
+
+    private final PagingService pagingService;
+
+    @Autowired
+    public PagingTest(PagingService pagingService) {
+        this.pagingService = pagingService;
+    }
+
+    @Test
+    void getTotalCountTest() {
+        // given
+        int count = 13;
+
+        // when
+        int totalCount = pagingService.getTotalCountOfStudyList();
+
+        // then
+        assertThat(count).isEqualTo(totalCount);
+    }
 }
