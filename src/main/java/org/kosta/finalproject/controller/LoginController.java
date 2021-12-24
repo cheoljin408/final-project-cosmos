1b3f94803e2e19cb04b38a2a5d04d8e373e2102b
Yong Lee <nogy21@gmail.com>
2021-12-24 14:27:08 +0900

    COSMOS-78 기존 코드 복구(삭제했던 코드 주석 처리)

--- src/main/java/org/kosta/finalproject/controller/LoginController.java
+++ src/main/java/org/kosta/finalproject/controller/LoginController.java
@@ -5,22 +5,18 @@ import org.springframework.core.ResolvableType;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
 public class LoginController {
     private static final String authorizationRequestBaseUri = "oauth2/authorization";
-    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+    // Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     @SuppressWarnings("unchecked")
     @GetMapping("/login")
-    public String getLoginPage(Model model) throws Exception {
+    public String getLoginPage() throws Exception {
         Iterable<ClientRegistration> clientRegistrations = null;
         ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
                 .as(Iterable.class);
@@ -28,6 +24,12 @@ public class LoginController {
                 ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
             clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
         }
+        assert clientRegistrations != null;
+        /*clientRegistrations.forEach(registration ->
+                oauth2AuthenticationUrls.put(registration.getClientName(),
+                        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);*/
+
         return "oauth-login";
     }
 }
