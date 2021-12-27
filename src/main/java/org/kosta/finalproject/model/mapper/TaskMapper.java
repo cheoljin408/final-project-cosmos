e1034aa6b5d395ba21b4787733ce13008aa3afc1
KimJoongHyeok <moon960427@gmail.com>
2021-12-27 12:11:24 +0900

    COSMOS-90 과제공지 등록 페이지 구현 및 기능 구현

--- src/main/java/org/kosta/finalproject/model/mapper/TaskMapper.java
+++ src/main/java/org/kosta/finalproject/model/mapper/TaskMapper.java
@@ -1,11 +1,22 @@
 package org.kosta.finalproject.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.kosta.finalproject.model.domain.TaskDTO;
+import org.kosta.finalproject.model.domain.UploadFile;
 
 import java.util.List;
 
 @Mapper
 public interface TaskMapper {
     List<TaskDTO> getRecentTaskList(int studyNo);
+
+    void registerTask(@Param("studyNo")int studyNo, @Param("email")String email, @Param("taskTitle") String taskTitle, @Param("taskContent")String taskContent);
+
+    int getTaskNoWhenRegister();
+
+    void registerAttachFile(@Param("attachFile")UploadFile attachFile, @Param("type")String type, @Param("taskNo")int taskNo);
+
+    void registerStoreImage(@Param("storeImage")UploadFile storeImage, @Param("type") String img, @Param("taskNo") int taskNo);
+
 }
--- src/main/java/org/kosta/finalproject/service/TaskService.java
+++ src/main/java/org/kosta/finalproject/service/TaskService.java
@@ -1,9 +1,14 @@
 package org.kosta.finalproject.service;
 
 import org.kosta.finalproject.model.domain.TaskDTO;
+import org.kosta.finalproject.model.domain.TaskFormDTO;
+import org.kosta.finalproject.model.domain.UploadFile;
 
 import java.util.List;
 
 public interface TaskService {
     List<TaskDTO> getRecentTaskList(int studyNo);
+
+    //과제 공지 등록
+    int registerTask(int studyNo, String email, TaskFormDTO taskFormDTO, List<UploadFile> attachFiles, List<UploadFile> uploadFiles);
 }
