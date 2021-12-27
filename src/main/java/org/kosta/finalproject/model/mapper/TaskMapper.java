e973de64b03963ff4d17c4b19223eefc8fc77f16
Joonkb <byeongjunkim@naver.com>
2021-12-27 13:23:49 +0900

    COSMOS-91 과제 공지 상세페이지
    MyBatis SQL 문 작업 및 Mapper 인터페이스 구현

--- src/main/java/org/kosta/finalproject/model/mapper/TaskMapper.java
+++ src/main/java/org/kosta/finalproject/model/mapper/TaskMapper.java
@@ -2,10 +2,16 @@ package org.kosta.finalproject.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.kosta.finalproject.model.domain.TaskDTO;
+import org.kosta.finalproject.model.domain.UploadFile;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface TaskMapper {
     List<TaskDTO> getRecentTaskList(int studyNo);
+
+    Map<String, Object> getTaskDetailByTaskNo(int taskNo);
+
+    List<UploadFile> findFilesById(String file, int taskNo);
 }
