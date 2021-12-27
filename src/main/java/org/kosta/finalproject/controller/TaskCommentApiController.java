a39f804b10273c27fa6908b53406def9e0937cca
Joonkb <byeongjunkim@naver.com>
2021-12-27 17:33:33 +0900

    COSMOS-101 과제 댓글 삭제하기 기능 Controller 구현
    - Controller 구현

--- src/main/java/org/kosta/finalproject/controller/TaskCommentApiController.java
+++ src/main/java/org/kosta/finalproject/controller/TaskCommentApiController.java
@@ -1,9 +1,17 @@
 package org.kosta.finalproject.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.kosta.finalproject.model.domain.StudyCommentDTO;
 import org.kosta.finalproject.service.TaskCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -21,4 +29,14 @@ public class TaskCommentApiController {
     // 과제 제출 수정
     
     // 과제 제출 삭제
+    @DeleteMapping("/api/deleteTaskComment/{submitNo}")
+    public String deleteTaskComment(@RequestBody int submitNo, @PathVariable int taskNo, Model model) {
+        System.out.println("submit = " + submitNo);
+        taskCommentService.deleteTaskComment(submitNo);
+
+        List<SubmitCommentDTO> allTaskCommentList = taskCommentService.getAllTaskCommentList(taskNo);
+        model.addAttribute("studyCommentList", allTaskCommentList);
+
+        return "fragments/task-submit-comment :: fragment-task-submit-comment";
+    }
 }
