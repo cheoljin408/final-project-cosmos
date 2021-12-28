593a45e0332fcb394b5b000a9825dbc84053f23c
KimJoongHyeok <moon960427@gmail.com>
2021-12-28 15:34:17 +0900

    COSMOS-104 과제공지 페이징 TEST코드 추가

--- src/test/java/org/kosta/finalproject/PagingTest.java
+++ src/test/java/org/kosta/finalproject/PagingTest.java
@@ -2,6 +2,7 @@ package org.kosta.finalproject;
 
 import org.junit.jupiter.api.Test;
 import org.kosta.finalproject.model.domain.NoticeDTO;
+import org.kosta.finalproject.model.domain.TaskDTO;
 import org.kosta.finalproject.service.PagingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,6 +46,23 @@ public class PagingTest {
         //then
         for (NoticeDTO noticeDTO : noticeList) {
             System.out.println(noticeDTO);
+        }
+
+    }
+
+    @Test
+    void geTaskListByPageNo(){
+        //given
+        int startRowNumber = 1;
+        int endRowNumber = 40;
+        int studyNo = 25;
+
+        //when
+        List<TaskDTO> taskList = pagingService.getTaskListByPageNo(studyNo,startRowNumber,endRowNumber);
+
+        //then
+        for (TaskDTO taskDTO : taskList) {
+            System.out.println(taskDTO);
         }
 
     }
